6ddaf69ebd6ebd79fed86a28b5f06dcf736afa0c
vsobolev <vsobolev@rbc.ru>
2022-09-22 21:11:21 +0300

    Убрано фиктивное сообщение для теста

--- receiver/receiver.py
+++ receiver/receiver.py
@@ -23,35 +23,10 @@ class Receiver:
         self.subscription_manager: Optional[SubscriptionManager] = None
         self.xrpl_websocket_client: Optional[AsyncWebsocketClient] = None
 
-    def __mess(self):
-        return {
-            "transaction": {
-                "Account": "r4k4ntDnrjvhvgJNQDW6pyY74qhcsZPKpX",
-                "Amount": "3041556",
-                "Destination": "rEb8TK3gBgk5auZkwc6sHnwrGVJH8DuaLh",
-                "Fee": "5015",
-                "Flags": 2147483648,
-                "LastLedgerSequence": 31226128,
-                "Sequence": 20878584,
-                "SigningPubKey": "031BCEF5A39E6523B839D5D606362A9D6C07ECF5ABA5D1AEF8A55C271CB91B8E05",
-                "TransactionType": "Payment",
-                "TxnSignature": "3045022100AF2CBD0BC760668104AE27C2A819262E84FE8D006274053F602C20E5768E7D000220443310385CC1236783DC391B582A8C60B1E3EB3FCB1230E779885B5FF23F9AAE",
-                "date": 716654231,
-                "hash": "B17BDDE564453750DA1A22158AAE6F43ADB6DEC6B6BA8E6E66DE2BB8FB62E97D",
-                "inLedger": 31226007,
-                "ledger_index": 31226007
-            },
-            'engine_result': 'tesSUCCESS',
-            "validated": True,
-            'status': 'success',
-            'type': 'transaction'
-        }
-
     async def _xrpl_listener(self, client):
         """Получение сообщений от XRPL, на данный момент используется для отправки сообщений на создание транзакции"""
         async for message in client:
             logger.info(f'Receive XRPL mess: {message}')
-            message = self.__mess()
             if message.get('type') == 'transaction' and message.get('engine_result') == 'tesSUCCESS':
                 # Отправить сообщение на создание транзакции
                 logger.info(f'Get payment from XRPL: {message}')
