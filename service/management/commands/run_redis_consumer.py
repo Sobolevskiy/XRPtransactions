08273c8443e9215c74495a14e3f045548acd1c20
vsobolev <vsobolev@rbc.ru>
2022-09-22 22:30:02 +0300

    Добавлено логирование в redis consumer

--- service/management/commands/run_redis_consumer.py
+++ service/management/commands/run_redis_consumer.py
@@ -7,13 +7,20 @@ from django.conf import settings
 from service.redis_management import RedisWorker
 from service import tasks
 
+logging.basicConfig(level="INFO")
 logger = logging.getLogger('default')
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        with redis.Redis(settings.REDIS['HOST'], port=settings.REDIS['PORT']) as redis_cli:
-            for message in RedisWorker(redis_cli, 'payments'):
-                logger.info(f'Receive message from Redis: {message}')
-                tasks.process_payment.apply_async(args=[message])
+        logger.info('Starting redis consumer')
+        try:
+            with redis.Redis(settings.REDIS['HOST'], port=settings.REDIS['PORT']) as redis_cli:
+                for message in RedisWorker(redis_cli, 'payments'):
+                    logger.info(f'Receive message from Redis: {message}')
+                    tasks.process_payment.apply_async(args=[message])
+        except Exception as e:
+            redis_cli.close()
+            logger.error(f'Error while running redis consumer: {e}')
+            raise e
